cd6ec38d52b6bf4d94e0fbf5a0c7a416e655e76c
LeonardoMiguel04 <159266494+LeonardoMiguel04@users.noreply.github.com>
2024-09-19 12:13:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcula(){
+    const T = parseFloat(prompt("quanto tempo de aluguel?"));
+    const P = parseFloat(prompt("Qual a alíquota?"));
+    const E = parseFloat(prompt("Qual o valor do aluguel?"));
+    const I = parseFloat(prompt("qual o tipo de aluguel?"));  /* IPCA e o IGP-M */
+    let R = P*(1+T);
+
+
+
+
+
+
+
+
+
+
+    
+}
